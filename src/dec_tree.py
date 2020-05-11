c1a39246b2d531ac33e9a901fa1dac9abff8388e
Minglewdongle <60474937+Minglewdongle@users.noreply.github.com>
2020-05-11 16:25:26 -0400

    altered values of the models to increase accuracy

--- src/dec_tree.py
+++ src/dec_tree.py
@@ -4,8 +4,8 @@ import numpy as np
 from sklearn import tree
 
 class dec_tree():
-    def __init__(self):
-        self.clf=tree.DecisionTreeClassifier()
+    def __init__(self,criterion='gini',splitter='best',max_depth=None,min_samples_split=2,min_samples_leaf=1):
+        self.clf=tree.DecisionTreeClassifier(criterion,splitter,max_depth,min_samples_split,min_samples_leaf)
 
     def setUp(self,matrix,values):
         self.clf.fit(matrix, values)
--- src/main.py
+++ src/main.py
@@ -7,6 +7,12 @@ import pandas as pd
 def kfold(matrix,k,values,clf):
     partition_size=int(np.shape(matrix)[0]/k)
     matrix=matrix.tolist()
+
+
+    average=0
+
+
+
     for i in range(k):
         part_start=i*partition_size
         part_end=(i+1)*partition_size
@@ -36,14 +42,16 @@ def kfold(matrix,k,values,clf):
                 print(res[x])
                 print(part_cls_val[x])
                 '''
-        print(counter)
+        average+=counter/partition_size
+        
+        if counter/partition_size>=.65:
+            print(counter/partition_size)
 
+        else:
+            print(counter/partition_size)
 
-
-
-
-
-
+            
+    print("average:"+str(average/k))
 
 
     
@@ -73,7 +81,7 @@ def main():
     preprocessor.preprocess(f)
     preprocessor.cleanUnfilled()
 
-    values = split_class(preprocessor.getColumn("Lifeexpectancy"), 44, 90, k=4)
+    values = split_class(preprocessor.getColumn("Lifeexpectancy"), 44, 90, k=5)
 
     '''
     print(np.size(values,0))
@@ -86,12 +94,16 @@ def main():
     for x in preprocessor.getMatrix()[0]:
         print(int(x))
     '''
+    print("tree")
+    clf=dec_tree(max_depth=5,min_samples_split=3)
+    kfold(preprocessor.getMatrix(),5,values,clf)
 
-    clf=dec_tree()
-    kfold(preprocessor.getMatrix(),3,values,clf)
+
     
-    clf=NeuralNetwork()
-    kfold(preprocessor.getMatrix(),3,values,clf)
+    clf=NeuralNetwork( activation="tanh",hidden_layer_sizes = (1000,100))
+    print("neural net")
+    kfold(preprocessor.getMatrix(),10,values,clf)
+
     
     
 
